af00515909a2a8ceb22af60cc80dcd88529fadff
dhs262003 <dhs262003@gmail.com>
2020-04-20 20:16:31 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,28 @@
+function clicked() {
+
+	var user = document.getElementById('username');
+	var pass = document.getElementById('password');
+	
+	var coruser = "Dhruvshah";
+	var corpass = "1991042";
+	
+	if(user.value == coruser) {
+	
+		if(pass.value == corpass) {
+		
+			window.alert("You are logged in as " + user.value);
+			window.open("../Project/supercell.html");
+		
+		} else {
+		
+			window.alert("Incorrect username or password!");
+		
+		}
+	
+	} else {
+	
+			window.alert("Incorrect username or password!");
+	
+	}
+
+}
